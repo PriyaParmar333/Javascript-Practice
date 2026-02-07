9f5253e4a0a482b8db43fb64238cb3183f2b7ce2
Priya Parmar <priyaparmar5526@gmail.com>
2026-02-07 22:53:02 +0530

    Add files via upload

---
+++ PopCorn.js
@@ -0,0 +1,16 @@
+let size="L";
+if(size==="XL") 
+{
+    console.log("Price is 250 rupees")
+}
+else if(size==="L")
+{
+    console.log("the price is 200 rupees")
+}
+else if(size==="M")
+{
+    console.log("the price is 100 rupees")
+}
+else {
+    console.log("price is 50 rupees")
+}
