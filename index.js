63a07576c623613edbb8827c5843a669f4fe216b
Priya Parmar <priyaparmar5526@gmail.com>
2026-02-11 23:22:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let pencilPrice=10;
+let erasorPrice=5;
+// console.log("the total price is:",pencilPrice+erasorPrice,"rupees");
+// let output="the total price is : "+(pencilPrice+erasorPrice)+"rupees";
+// console.log(output);
+let output=`the total price is : ${pencilPrice+erasorPrice} rupees`;
+console.log(output);
+alert("Something is wrong! Please try again...");
+
+let firstName=prompt("enter your first name");
+let lastName=prompt("enter your last name");
+// console.log("Welcome ",firstName," ",lastName,"!");
+let msg= "Welcome "+ firstName +" "+lastName + "!";
+alert(msg);
